667f7a331e27b10e7b97cfe6cec149340cac11b1
Yusuke Furuya <furuya0102@g-gent.com>
2017-01-16 00:48:05 +0900

    phpinfo削除

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,5 @@
         <a href="{{ route("reservation.index") }}" class="btn btn-default">予約</a>
         <a href="{{ route("facility.index") }}" class="btn btn-default">設備マスタ</a>
     </div>
-    <p>{{ phpinfo() }}</p>
 </div>
 @endsection
